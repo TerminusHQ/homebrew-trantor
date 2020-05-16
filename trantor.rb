f467d011d0522e7afb55ab573d50640e36b79d52
Anson Chan <anson0370@gmail.com>
2020-05-16 18:33:01 +0800

    update trantor cli version [0.0.10], trantor commit[97746698c426fbff04fc32d82ba79c7eb34b7d72]

--- trantor.rb
+++ trantor.rb
@@ -1,9 +1,9 @@
 class Trantor < Formula
   desc "Terminus Trantor CLI"
   homepage "https://www.terminus.io/"
-  url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.0.9.tar.gz"
-  version "0.0.9"
-  sha256 "5bf04ef695c77bfdb3e61a144558886a27646de1284602d898b1ee5231d0143c"
+  url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.0.10.tar.gz"
+  version "0.0.10"
+  sha256 "a659ce8c04625990cab126e234feb45bb934a47b84b309c8ef5efd79557e6fa3"
 
 #   depends_on "docker"
 
@@ -16,7 +16,7 @@ class Trantor < Formula
         JAVACMD="$JAVA_HOME/bin/java"
       fi
       export TRANTOR_HOME="#{prefix}"
-      export TRANTOR_CLI_VERSION="0.0.9"
+      export TRANTOR_CLI_VERSION="0.0.10"
       exec "$JAVACMD" -jar "#{libexec}/trantor-cli.jar" "$@"
     EOS
   end
