0f0b414f7efb6d95d4c46fb85d30d2e119b54981
ronggeng <gengrong.gr@alibaba-inc.com>
2020-10-29 16:33:02 +0800

    update trantor cli version to [0.13.2], trantor commit[2311babd3518520df56b8860a872f70f979d5616]

--- trantor.rb
+++ trantor.rb
@@ -3,7 +3,7 @@ class Trantor < Formula
   homepage "https://www.terminus.io/"
   url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.13.2.tar.gz"
   version "0.13.2"
-  sha256 "682812af66e04751a955b838ed26813261624c56a7817a5ed5668ac1f32cb9bb"
+  sha256 "56789ca7d598830246a07198b7b6af63e6c06206913dd4e20f46ac008578f233"
 
 #   depends_on "docker"
 
