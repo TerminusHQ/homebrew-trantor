93f367984b5365af3c9fd83a064fceebf2a3c7f6
ronggeng <gengrong.gr@alibaba-inc.com>
2021-02-23 13:55:50 +0800

    update trantor cli version to [0.16.3], trantor commit[0b3880e31ab5a856a1a585bb04373dd396b19104]

--- trantor.rb
+++ trantor.rb
@@ -1,9 +1,9 @@
 class Trantor < Formula
   desc "Terminus Trantor CLI"
   homepage "https://www.terminus.io/"
-  url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.16.2.tar.gz"
-  version "0.16.2"
-  sha256 "21e658341b557d14a01250acc01ef5a91d717879903db96077e756e828309244"
+  url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.16.3.tar.gz"
+  version "0.16.3"
+  sha256 "d1630bbc1f30bcc647876d2694aa74b09bd14adb1b53b05d604cb39ca34b8cb6"
 
 #   depends_on "docker"
 
@@ -16,7 +16,7 @@ class Trantor < Formula
         JAVACMD="$JAVA_HOME/bin/java"
       fi
       export TRANTOR_HOME="#{prefix}"
-      export TRANTOR_CLI_VERSION="0.16.2"
+      export TRANTOR_CLI_VERSION="0.16.3"
       exec "$JAVACMD" -jar "#{libexec}/trantor-cli.jar" "$@"
     EOS
   end
